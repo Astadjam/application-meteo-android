2950d17d149ba892ff773c5e839a414ac36df464
Astadjam <goggopetel93@yahoo.fr>
2024-12-29 13:50:20 +0100

    Refactor de l'instantiation de retrofit

--- app/src/main/java/com/asta/meteoapp/model/HomeModel.kt
+++ app/src/main/java/com/asta/meteoapp/model/HomeModel.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.asta.meteoapp.api.geocodingAPI.GeocodingRequests
+import com.asta.meteoapp.api.meteofranceAPI.MeteofranceRequests
 import com.asta.meteoapp.datacontracts.WeatherData
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -16,23 +18,30 @@ class HomeModel: ViewModel() {
     private val weatherList = mutableStateListOf<WeatherData>()
     private val weatherFavoriteList = mutableStateListOf<WeatherData>()
     private var task: Job? = null
+    private var geocodingRequests: GeocodingRequests?=null
+    private var meteofranceRequests: MeteofranceRequests?=null
 
     fun getInput() = this.input
     fun getWeatherList() = this.weatherList
     fun getWeatherFavoriteList() = this.weatherFavoriteList
 
     fun searchFromInput(input: String, context: Context){
+        if(geocodingRequests === null){
+            geocodingRequests = com.asta.meteoapp.api.geocodingAPI.createRetrofit(context)
+        }
+        if(meteofranceRequests === null){
+            meteofranceRequests = com.asta.meteoapp.api.meteofranceAPI.createRetrofit(context)
+        }
         weatherList.clear()
         task?.cancel()
         task = viewModelScope.launch {
             delay(300)
             try {
-                var resultsSearchFromInput = com.asta.meteoapp.api.geocodingAPI.createRetrofit(context).getLocationData(input)
+                var resultsSearchFromInput = geocodingRequests!!.getLocationData(input)
 
                 if(resultsSearchFromInput !== null){
-                    var instanceOfInterface = com.asta.meteoapp.api.meteofranceAPI.createRetrofit(context)
                     var resultWeatherDataList = resultsSearchFromInput.results.mapNotNull {
-                        var weatherData = instanceOfInterface.getWeather(it.longitude, it.latitude)?.convertToWeatherData()
+                        var weatherData = meteofranceRequests!!.getWeather(it.longitude, it.latitude)?.convertToWeatherData()
 
                         weatherData?.city=it.name
                         weatherData?.country=it.country
@@ -47,12 +56,15 @@ class HomeModel: ViewModel() {
     }
 
     fun searchFromGeolocation(longitude: Double, latitude: Double, context: Context){
+        if(meteofranceRequests === null){
+            meteofranceRequests = com.asta.meteoapp.api.meteofranceAPI.createRetrofit(context)
+        }
         weatherList.clear()
         task?.cancel()
         task = viewModelScope.launch {
             delay(300)
             try {
-                var weatherData = com.asta.meteoapp.api.meteofranceAPI.createRetrofit(context).getWeather(longitude, latitude)
+                var weatherData = meteofranceRequests!!.getWeather(longitude, latitude)
                 if(weatherData !== null){
                     weatherList.add(weatherData.convertToWeatherData())
                 }
